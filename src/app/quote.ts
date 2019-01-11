086382b19c986fb591cebcb36d9c6f90836a1a2a
blackrose <wahomekelving@gmail.com>
2019-01-11 11:45:17 +0300

    Added component quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {
-  constructor(public id:number, public name:string, public author:string, public username:string)
+  constructor(public id:number, public name:string, public author:string, public username:string){}
 }
